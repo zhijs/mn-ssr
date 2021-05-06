235fc4f6752f5198914f01ef26243eb3125fac78
leozyang <leozyang@tencent.com>
2021-05-06 11:26:19 +0800

    feaet(entry): add entry

--- README.md
+++ README.md
@@ -3,7 +3,6 @@ mini react ssr framework
 
 ### 命令
 ```shell
-
 npm run dev # 开发模式
 npm run build # 编译 SSR 代码和 SPA 代码
 npm run build:client #只编译 SPA 代码
---
+++ entry/server.js
@@ -0,0 +1,12 @@
+/**
+ * 导出一个中间件
+ */
+import app from '../src/app'
+
+const renderHtml = async () => {
+  const fetch  = app.fetch
+  // fetch 方法存在的情况下
+  if (fetch && typeof fetch === 'function') {
+    
+  }
+}
